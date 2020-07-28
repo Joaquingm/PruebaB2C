201956b80afd4e43d14be6f80597c12a2f27f1e1
russo- russo- <uo@uniovi.es>
2020-07-28 11:48:53 +0200

    Feature 4

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 let oJSONTest = "";
+let bOrder = false;
 
 createForm();
 
 fetch("https://raw.githubusercontent.com/ITBconsult/json-data/master/data.json")
   .then(response => response.json())
-  .then(json => appendData(json));
+  .then(json => {
+      appendData(json);
+      oJSONTest = json;
+});
 
 
   function createForm() {
@@ -17,7 +21,8 @@ fetch("https://raw.githubusercontent.com/ITBconsult/json-data/master/data.json")
     let oTitleText = document.createTextNode("Update Data Form");
     document.getElementById("formContainer").appendChild(oTitle);
     oTitle.appendChild(oTitleText);
-  
+
+    createSortButtons();
 
     let oLabel = document.createElement("LABEL");
     let oText = document.createTextNode("Name : ");
@@ -72,37 +77,130 @@ fetch("https://raw.githubusercontent.com/ITBconsult/json-data/master/data.json")
     oInput.setAttribute("value", "Insert age");
     document.getElementById("dataForm").appendChild(oInput);
 
+    insertLineBreak(document.getElementById("dataForm"));
+
     let oButton = document.createElement("BUTTON");
     let oButtonText = document.createTextNode("UPDATE");
     oButton.appendChild(oButtonText);
     document.body.appendChild(oButton);
-    addEventListener("click", function(){alert("Record UPDATED")});
+    oButton.addEventListener("click", function(){alert("Record UPDATED")});
 
-    insertLineBreak(document.getElementById("dataForm"));
-
-    
-
-
-    
-
-    
-
-    
+    insertLineBreak(document.getElementById("dataForm")); 
   }
 
-  function appendData(data) {
-    var mainContainer = document.getElementById("mainContainer");
-    for (var i = 0; i < data.length; i++) {
-      var oDiv = document.createElement("div");
-      oDiv.innerHTML = `Name: ${data[i].name} - City: ${data[i].city} - Color: ${data[i].color} - Age: ${data[i].age}`;
+  function appendData(oData) {
+    let mainContainer = document.getElementById("mainContainer");
+    
+    for (let i = 0; i < oData.length; i++) {
+        let oDiv = document.createElement("div");
+      oDiv.innerHTML = `Name: ${oData[i].name} - City: ${oData[i].city} - Color: ${oData[i].color} - Age: ${oData[i].age}`;
       mainContainer.appendChild(oDiv);
     }
   }
 
   function insertLineBreak(container){
-    var oBR = document.createElement('br');
+    let oBR = document.createElement('br');
     container.appendChild(oBR);
   }
 
+  function sortListByName(json){
+      
+    if(bOrder) {
+        bOrder = false;
+        oJSONTest.sort((a, b) => (a.name > b.name) ? 1 : -1);
+    }
+    else {
+        oJSONTest.sort((a, b) => (a.name < b.name) ? 1 : -1);
+        bOrder = true;
+    }
 
- // testjson.forEach(element => console.log(element));
+    let buttonContainer = document.getElementById("mainContainer")
+    buttonContainer.innerHTML = "";
+    
+    printList(oJSONTest);
+  }
+
+  function sortListByCity(json){
+    if(bOrder) {
+        oJSONTest.sort((a, b) => (a.city > b.city) ? 1 : -1);
+        bOrder = false;
+    }
+    
+    else {
+        oJSONTest.sort((a, b) => (a.city < b.city) ? 1 : -1);
+        bOrder = true;
+    }
+
+    let mainContainer = document.getElementById("mainContainer").innerHTML = "";
+    printList(oJSONTest);
+  }
+
+  function sortListByColor(json){
+    if(bOrder) {
+        oJSONTest.sort((a, b) => (a.color > b.color) ? 1 : -1);
+        bOrder = false;
+    }
+    else {
+        oJSONTest.sort((a, b) => (a.color < b.color) ? 1 : -1);
+        bOrder = true;
+    }
+
+    let mainContainer = document.getElementById("mainContainer").innerHTML = "";
+    printList(oJSONTest);
+  }
+
+  function sortListByAge(json){
+    if(bOrder) {
+        oJSONTest.sort((a, b) => (a.age > b.age) ? 1 : -1);
+        bOrder = false;
+    }
+    else {
+        oJSONTest.sort((a, b) => (a.age < b.age) ? 1 : -1);
+        bOrder = true;
+    }
+    let mainContainer = document.getElementById("mainContainer").innerHTML = "";
+    printList(oJSONTest);
+  }
+
+  function printList(){
+    createSortButtons();
+    for (let i = 0; i < oJSONTest.length; i++) {
+        let oDiv = document.createElement("div");
+      oDiv.innerHTML = `Name: ${oJSONTest[i].name} - City: ${oJSONTest[i].city} - Color: ${oJSONTest[i].color} - Age: ${oJSONTest[i].age}`;
+      mainContainer.appendChild(oDiv);
+    }
+  }
+
+  function createSortButtons(){
+    let oDiv = document.createElement("div");
+    oDiv.id = "sortButtonsContainer";
+    //let sortButtonsContainer = document.getElementById("sortButtonsContainer");
+    mainContainer.appendChild(oDiv);
+
+
+    let oButton = document.createElement("BUTTON");
+    let oButtonText = document.createTextNode("Sort by name");
+    oButton.appendChild(oButtonText);
+    sortButtonsContainer.appendChild(oButton);
+    oButton.addEventListener("click", sortListByName);
+    
+    oButton = document.createElement("BUTTON");
+    oButtonText = document.createTextNode("Sort by city");
+    oButton.appendChild(oButtonText);
+    sortButtonsContainer.appendChild(oButton);
+    oButton.addEventListener("click", sortListByCity);
+
+    oButton = document.createElement("BUTTON");
+    oButtonText = document.createTextNode("Sort by color");
+    oButton.appendChild(oButtonText);
+    sortButtonsContainer.appendChild(oButton);
+    oButton.addEventListener("click", sortListByColor);
+
+    oButton = document.createElement("BUTTON");
+    oButtonText = document.createTextNode("Sort by age");
+    oButton.appendChild(oButtonText);
+    sortButtonsContainer.appendChild(oButton);
+    oButton.addEventListener("click", sortListByAge);
+    insertLineBreak(document.getElementById("mainContainer")); 
+
+  }
